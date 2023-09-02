1fb63da7d0bac02b15b6b675bce0ec83b6489e9a
Evertiin <evertonvendramine@gmail.com>
2023-09-02 10:00:47 -0300

    api node

---
+++ node-str/src/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const config = require('./config');
+
+const app = express();
+const router = express.Router();
